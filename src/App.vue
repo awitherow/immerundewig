d774409430d276a4023a8dd076a46e7f21c9f121
Austin Witherow <au.witherow@gmail.com>
2016-10-14 18:23:56 +0200

    adds toggle overlay

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
   <div id="site-container">
     <div id="header">
       <button
-        v-on:click="menuOpen = !menuOpen"
-        v-show="!menuOpen"
-        class="reset"
+        v-on:click="overlay = !overlay"
+        v-show="!overlay"
+        class="menu"
         >MENÜ</button>
+    </div>
+    <div id="overlay" v-show="overlay">
+      <button
+        v-on:click="overlay = !overlay"
+        v-show="overlay"
+        class="close"
+        />
     </div>
     <Introduction />
   </div>
@@ -18,7 +25,7 @@ export default {
     Introduction
   },
   data: () => ({
-    menuOpen: false
+    overlay: false
   })
 }
 </script>
@@ -27,6 +34,58 @@ export default {
   @import './common/styles/index.scss';
 
   #header {
+    position: fixed;
+    left: 15px;
+    top: 35px;
+
+    button {
+      font-size: 16px;
+      padding-bottom: 2px;
+
+      &.menu {
+        border: none;
+        padding: 0;
+        border-bottom: 2px solid $gold;
+      }
+    }
+  }
+
+  #overlay {
     position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: black;
+
+    button {
+      border: none;
+      &.close {
+        position: absolute;
+        right: 10px;
+        top: 30px;
+        width: 32px;
+        height: 32px;
+
+        &:before,
+        &:after {
+          position: absolute;
+          left: 15px;
+          top: 0;
+          content: ' ';
+          height: 33px;
+          width: 2px;
+          background-color: $gold;
+        }
+
+        &:before {
+          transform: rotate(45deg);
+        }
+
+        &:after {
+          transform: rotate(-45deg);
+        }
+      }
+    }
   }
 </style>
