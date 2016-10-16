8befab3ba7403bf40c0cf9dc802f5ff3329ac0b2
Austin Witherow <au.witherow@gmail.com>
2016-10-16 21:41:29 +0200

    isolates all of menulinks

--- src/ImmerUndEwig.vue
+++ src/ImmerUndEwig.vue
@@ -3,21 +3,13 @@
 
     <Header
       :toggleMenuLinks="toggleMenuLinks"
-      :menuLinks="menuLinks"
+      :menuLinksActive="menuLinksActive"
       />
 
-    <transition name="fade">
-      <div class="overlay" id="menu-links-overlay" v-show="menuLinks">
-        <MenuLinks
-          v-bind:onClickLink="toggleMenuLinks"
-          />
-        <button
-          v-on:click="toggleMenuLinks"
-          v-show="menuLinks"
-          class="close"
-          />
-      </div>
-    </transition>
+    <MenuLinks
+      :menuLinksActive="menuLinksActive"
+      :toggleMenuLinks="toggleMenuLinks"
+      />
 
     <Introduction />
     <Studio />
@@ -38,67 +30,16 @@ export default {
     Header
   },
   data: () => ({
-    menuLinks: false
+    menuLinksActive: false
   }),
   methods: {
     toggleMenuLinks (contentDesired) {
-      this.menuLinks = !this.menuLinks
+      this.menuLinksActive = !this.menuLinksActive
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './common/styles/index.scss';
 
-  #menu-links-overlay {
-    ul {
-      padding: 0;
-      margin: 0;
-      text-transform: uppercase;
-      text-align: center;
-
-      li {
-        list-style: none;
-        font-size: 18px;
-        padding-bottom: 5px;
-
-        a {
-          color: $gold;
-          text-decoration: none;
-        }
-      }
-    }
-
-    button {
-      border: none;
-
-      &.close {
-        position: absolute;
-        right: 10px;
-        top: 30px;
-        width: 32px;
-        height: 32px;
-
-        &:before,
-        &:after {
-          position: absolute;
-          left: 15px;
-          top: 0;
-          content: ' ';
-          height: 33px;
-          width: 2px;
-          background-color: $gold;
-        }
-
-        &:before {
-          transform: rotate(45deg);
-        }
-
-        &:after {
-          transform: rotate(-45deg);
-        }
-      }
-    }
-  }
 </style>
--- src/components/elements/MenuLinks.vue
+++ src/components/elements/MenuLinks.vue
@@ -1,20 +1,26 @@
 <template>
-  <ul>
-    <li v-for="link in linkList">
-      <a
-        v-bind:href="link.href"
-        v-on:click="onClickLink"
-        >
-        {{ link.text }}
-      </a>
-    </li>
-  </ul>
+  <transition name="fade">
+    <div class="overlay" id="menu-links-overlay" v-show="menuLinksActive">
+      <ul>
+        <li v-for="link in linkList">
+          <a v-bind:href="link.href">
+            {{ link.text }}
+          </a>
+        </li>
+      </ul>
+      <button
+        v-on:click="toggleMenuLinks"
+        v-show="menuLinksActive"
+        class="close"
+        />
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'MenuLinks',
-  props: ['onClickLink'],
+  props: ['menuLinksActive', 'toggleMenuLinks'],
   data: () => ({
     linkList: [
       {
@@ -39,5 +45,56 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../../common/styles/index.scss';
 
+#menu-links-overlay {
+  ul {
+    padding: 0;
+    margin: 0;
+    text-transform: uppercase;
+    text-align: center;
+
+    li {
+      list-style: none;
+      font-size: 18px;
+      padding-bottom: 5px;
+
+      a {
+        color: $gold;
+        text-decoration: none;
+      }
+    }
+  }
+
+  button {
+    border: none;
+
+    &.close {
+      position: absolute;
+      right: 10px;
+      top: 30px;
+      width: 32px;
+      height: 32px;
+
+      &:before,
+      &:after {
+        position: absolute;
+        left: 15px;
+        top: 0;
+        content: ' ';
+        height: 33px;
+        width: 2px;
+        background-color: $gold;
+      }
+
+      &:before {
+        transform: rotate(45deg);
+      }
+
+      &:after {
+        transform: rotate(-45deg);
+      }
+    }
+  }
+}
 </style>
