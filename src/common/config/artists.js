0d56f1ba0d15186927a98408a0bf9cae068c0779
Austin Witherow <au.witherow@gmail.com>
2016-10-17 12:35:12 +0200

    adds basic artist config file

---
+++ src/common/config/artists.js
@@ -0,0 +1,20 @@
+const artists = [
+  {
+    'name': 'Susanne König',
+    'introduction_paragraphs': [
+      'Drown in Susanne Königs beautiful blend of blackwork and dotwork with her lovely illustrative tattoos.',
+      'If you have a knack for blackwork illustrations and whimsical designs, then we\'re going to warn you firsthand that you just might fall hard for Susanne Königs lovely tattoos.'
+    ],
+    contact: 'susanne@immerundewig-tattooing.de'
+  },
+  {
+    'name': 'Christian Hensen',
+    'introduction_paragraphs': [
+      'Drown in Susanne Königs beautiful blend of blackwork and dotwork with her lovely illustrative tattoos.',
+      'If you have a knack for blackwork illustrations and whimsical designs, then we\'re going to warn you firsthand that you just might fall hard for Susanne Königs lovely tattoos.'
+    ],
+    contact: 'susanne@immerundewig-tattooing.de'
+  }
+]
+
+export default artists
